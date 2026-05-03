7017f9d788d5be5e9195b7f70a19a8014893775f
khavnekaraniketTech <aniketkhavnekar.test@gmail.com>
2026-05-03 08:09:11 +0000

    new changes

---
+++ src/javaScript/functionAll.js
@@ -0,0 +1,12 @@
+// normal function 
+function myFunction(a, b) {
+    return a * b
+}
+
+const myFunction1 = function name1(x, y) {
+    return x + y;
+}
+
+const myFunction2 = (x, y) => {
+    return x + y
+}
